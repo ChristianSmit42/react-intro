6f9dd5a71c97ac5dafb27b1d13452de9c971bd76
Christian Smit <chrsmit@web.de>
2020-06-15 15:01:57 +0200

    Add css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from "./components/Header";
 import List from "./components/List"
-import Food from "./components/food";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import "./App.css";
 
 const beers = ["beer1", "beer2", "beer3", "beer42"];
 const animals = ["beaver", "cow", "pig", "rat"];
@@ -10,9 +12,11 @@ function App() {
   return (
     <div className="App">
         <Header>Animals and Beer</Header>
-        <List items={beers}></List>
-        <List items={animals}></List>
-        <Food className={"none"}>Kirsche</Food>
+        <Main>
+            <List items={beers}></List>
+            <List items={animals}></List>
+        </Main>
+        <Footer>made with React</Footer>
     </div>
   );
 }
