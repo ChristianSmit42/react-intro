ec1c4f7250aff319425d47f992071b59bd387695
Christian Smit <chrsmit@web.de>
2020-06-15 14:33:35 +0200

    Add food component returns p with class as props.className and content as props.children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header";
 import List from "./components/List"
+import Food from "./components/food";
 
 const beers = ["beer1", "beer2", "beer3", "beer42"];
 const animals = ["beaver", "cow", "pig", "rat"];
@@ -8,10 +9,10 @@ const animals = ["beaver", "cow", "pig", "rat"];
 function App() {
   return (
     <div className="App">
-        <Header>
-        </Header>
+        <Header>Animals and Beer</Header>
         <List items={beers}></List>
         <List items={animals}></List>
+        <Food className={"none"}>Kirsche</Food>
     </div>
   );
 }
