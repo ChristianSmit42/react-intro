ef5687a27aa4302b2666d301e510e0f8ace52fb1
Christian Smit <chrsmit@web.de>
2020-06-15 12:36:55 +0200

    Fix dependencies in App, Header, List, ListItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from "./components/Header";
+import List from "./components/List"
+
+const beers = ["beer1", "beer2", "beer3", "beer42"];
+const animals = ["beaver", "cow", "pig", "rat"];
 
 function App() {
   return (
     <div className="App">
         <Header>
-
         </Header>
+        <List items={beers}></List>
+        <List items={animals}></List>
     </div>
   );
 }
